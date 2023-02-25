b019131a21bdb4c3d696a53f827c144cbecb525c
Tolulope_xo <60390674+Tolulope-xo@users.noreply.github.com>
2023-02-25 17:27:04 +0000

    Implementation of a new Portfolio design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,6 @@
+import React, {useState} from 'react';
 import {HiBars2} from 'react-icons/hi2'
 import {AiOutlineClose} from 'react-icons/ai'
-import React, {useState} from 'react';
-
 import { Link } from "react-router-dom";
 
 const Nav = () => {
@@ -12,31 +11,10 @@ const Nav = () => {
   };
   return (
     <div>
-    {/* <div className="bg-black pb-[2rem]">
-      <div className="flex justify-between text-[white] items-center pt-[1rem]">
-      <div>
-        <h2 className="text-[22px] text-[gold]">Olatunji Tolulope O.</h2>
-      </div>
-      <div className="flex justify-between text-[20px] w-[40%]">
-      <Link to="/">
-      Home
-          </Link>
-          <Link to="/about" >
-      About
-          </Link>
-          <Link to="/projects">
-      Portfolio
-          </Link>
-      </div>
-    </div>
-    </div> */}
-
 <nav className="w-full  bg-black shadow">
 <div className="justify-between px-4 mx-auto md:items-center md:flex md:px-8">
   <div className="flex items-center justify-between gap-[2rem] py-3 md:py-5 md:block">
-    <a href="#">
       <h2 className="text-2xl m-[auto] w-100vw text-center md:text-left font-[Stix] md:m-0 text-white font-bold">Olatunji Tolulope O.</h2>
-    </a>
     <div className="block md:hidden">
       <button
         className="p-2 text-black"
@@ -56,7 +34,7 @@ const Nav = () => {
       className={` w-[20rem] md:flex-row md:items-center justify-self-center pb-3 mt-8 md:pb-0 md:mt-2 ${navbar ? 'block md:flex' : 'hidden md:flex'
         }`}
     >
-      <ul className="block md:flex items-center justify-between w-[20rem] space-y-8 md:space-y-0 block md:flex ">
+      <ul className="block md:flex items-center justify-between w-[20rem] space-y-8 md:space-y-0">
         <li>
       <Link to="/" onClick={handleToggle} className='md:border-b-2 md:border-[black] text-[white]'>
       Home
@@ -78,11 +56,8 @@ const Nav = () => {
           </Link>
           </li>
       </ul>
-
     </div>
-
   </div>
-  
 </div>
 </nav>
 </div>
